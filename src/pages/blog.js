32bcf877492f795b1a076169fe7dbf290273b25d
EvaSpessotto <eva@spessotto.com>
2019-03-04 14:20:51 +0100

    Add style to posts list

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,24 +1,42 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
+import { Container, Grid, Card } from 'semantic-ui-react'
 import Layout from '../components/layout'
 
 export default function BlogPage({ data }) {
   const { edges: posts } = data.allMarkdownRemark
   return (
     <Layout className="blog-posts">
-      {posts
-        .filter(post => post.node.frontmatter.title.length > 0)
-        .map(({ node: post }) => {
-          return (
-            <div className="blog-post-preview" key={post.id}>
-              <h1>
-                <Link to={post.frontmatter.path}>{post.frontmatter.title}</Link>
-              </h1>
-              <h2>{post.frontmatter.date}</h2>
-              <p>{post.excerpt}</p>
-            </div>
-          )
-        })}
+      <Container>
+        <Grid>
+          <Grid.Row>
+            <Grid.Column>
+              <h1>Blog</h1>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column>
+              <Card.Group>
+                {
+                  posts
+                    .filter(post => post.node.frontmatter.title.length > 0)
+                    .map(({ node: post }) => {
+                      return (
+                        <Card key={post.id} fluid>
+                          <Card.Content>
+                            <Card.Header as={Link} to={post.frontmatter.path}>{post.frontmatter.title}</Card.Header>
+                            <Card.Meta>{post.frontmatter.date}</Card.Meta>
+                            <Card.Description>{post.excerpt}</Card.Description>
+                          </Card.Content>
+                        </Card>
+                      )
+                    })
+                }
+              </Card.Group>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Container>
     </Layout>
   )
 }
