2bc2b19a789a1073ab765073f3ded9c2330d59c9
EvaSpessotto <eva@spessotto.com>
2019-03-04 14:06:35 +0100

    Change header (remove icon)

--- src/components/global/header.js
+++ src/components/global/header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Menu, Container, Icon } from 'semantic-ui-react';
+import { Menu, Container, Header } from 'semantic-ui-react';
 import { Link } from 'gatsby'
 
 
@@ -8,8 +8,7 @@ const Navigation = () => {
     <Menu borderless inverted stackable size="massive" style={{backgroundColor: '#424242'}}>
       <Container>
         <Menu.Item position="right" as={Link} to="/blog">
-          <Icon name="blogger b" circular inverted></Icon>
-          Blog
+          <Header inverted>Blog</Header>
         </Menu.Item>
       </Container>
     </Menu>
