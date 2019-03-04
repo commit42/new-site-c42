e3f9b3aabaacc5faeeeb474b23bb9a928848c5c1
EvaSpessotto <eva@spessotto.com>
2019-03-04 14:00:59 +0100

    Create post template

---
+++ src/pages/blog.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Container } from 'semantic-ui-react'
+import Layout from '../components/layout'
+
+const BlogPage = () => {
+  return ( 
+    <Layout>
+
+    </Layout>
+   );
+}
+ 
+export default BlogPage;
---
+++ src/templates/blog-post.js
@@ -0,0 +1,31 @@
+import React from "react"
+import { graphql } from "gatsby"
+import Layout from '../components/layout'
+
+export default function Template({ data }) {
+  const { markdownRemark: post } = data
+  return (
+    <Layout>
+      <div className="blog-post">
+        <h1>{post.frontmatter.title}</h1>
+        <div
+          className="blog-post-content"
+          dangerouslySetInnerHTML={{ __html: post.html }}
+        />
+      </div>
+    </Layout>
+  )
+}
+
+export const pageQuery = graphql`
+  query BlogPostByPath($path: String!) {
+    markdownRemark(frontmatter: { path: { eq: $path } }) {
+      html
+      frontmatter {
+        date
+        path
+        title
+      }
+    }
+  }
+`
