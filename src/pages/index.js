0dba8221dff6fa07aa32c56fd04efdca5621a94f
EvaSpessotto <eva@spessotto.com>
2019-03-04 15:16:49 +0100

    Test technos responsives

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,15 @@
 import React from "react";
 import 'semantic-ui-css/semantic.min.css'
-import { Image, Container, Grid, Header } from 'semantic-ui-react'
+import { Image, Container, Grid, Header, Responsive } from 'semantic-ui-react'
 import Layout from "../components/layout";
 import Navigation from '../components/navigation';
 import logo from '../../static/assets/logo-c42.png'
 
 const IndexPage = () => {
   return (
-    <div style={{backgroundColor: '#424242'}}>
+    <div style={{ backgroundColor: '#424242' }}>
       <Layout>
-        <Container style={{marginTop:"30vh", maxWidth:'50%'}}>
+        <Container style={{ marginTop: "30vh", maxWidth: '50%' }}>
           <Grid centered>
             <Grid.Row >
               <Grid.Column>
@@ -21,13 +21,21 @@ const IndexPage = () => {
                 <Navigation />
               </Grid.Column>
             </Grid.Row>
-            <Grid.Row style={{ maxWidth:'100%'}}>
-              <Grid.Column  textAlign="center">
+            <Grid.Row>
+              <Grid.Column textAlign="center">
                 <Header as="h1">
-                  <a href="https://reactjs.org/" target="_blank">#React</a>
-                  <a href="https://developers.google.com/web/progressive-web-apps/" target="_blank">#PWA</a>
-                  <a href="https://cakephp.org/" target="_blank">#CakePHP</a>
-                  <a href="https://magento.com/" target="_blank">#Magento</a>
+                  <Responsive>
+                    <a href="https://reactjs.org/" target="_blank">#React</a>
+                  </Responsive>
+                  <Responsive>
+                    <a href="https://developers.google.com/web/progressive-web-apps/" target="_blank">#PWA</a>
+                  </Responsive>
+                  <Responsive>
+                    <a href="https://cakephp.org/" target="_blank">#CakePHP</a>
+                  </Responsive>
+                  <Responsive>
+                    <a href="https://magento.com/" target="_blank">#Magento</a>
+                  </Responsive>
                 </Header>
               </Grid.Column>
             </Grid.Row>
