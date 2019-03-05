54cd0524e307e504b7018c00c6b3d6f4bdc8be95
EvaSpessotto <eva@spessotto.com>
2019-03-05 11:13:19 +0100

    Add footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,12 +1,13 @@
 import React from "react";
-import Header from './global/header'
+import Header from './global/header';
+import Footer from './global/footer';
 
 export default ({ children }) => (
-  <div style={{minHeight:'100vh'}}>
+  <div style={{minHeight:'100vh', display:'flex', flexDirection:'column'}}>
     <Header />
-      <div >
+      <div style={{flex:'1'}}>
         {children}
       </div>
-      
+    <Footer/>
   </div>
 )
