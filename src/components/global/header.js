6e67fdaa72736d3f5c99cc2be45db48a314c173f
EvaSpessotto <eva@spessotto.com>
2019-03-06 10:22:24 +0100

    Fix bug header

--- src/components/global/header.js
+++ src/components/global/header.js
@@ -1,45 +1,28 @@
 import React, { Component } from 'react';
 import './header.scss'
-import { Menu, Container, Image, Icon, Grid, Segment } from 'semantic-ui-react';
-import { Link, StaticQuery } from 'gatsby'
+import { Menu, Image, Icon, Grid } from 'semantic-ui-react';
+import { Link } from 'gatsby'
 import Logo from '../../../static/assets/logo-mini-c42-N&B-sansfond.png'
 
 
 const Header = (props) => {
   return (
-    <StaticQuery
-      query={graphql`
-        query {
-          allSitePage{
-            edges {
-              node {
-                path
-              }
-            }
-          }
-        }
-      `}
-      render={data => {
-        return (
-          <Menu borderless inverted size="massive" style={{backgroundColor: '#424242',borderRadius:'0',height:'103.406px'}}>
-            <Grid container>
+    <Menu borderless inverted size="massive" style={{ backgroundColor: '#424242', borderRadius: '0', height: '103.406px' }}>
+      <Grid container>
 
-              <Grid.Column mobile={10} computer={14}>
-                {props.path !== "/" && <Menu.Item as={Link} to="/" >
-                  <Image src={Logo} style={{ maxWidth: "5rem" }} />
-                </Menu.Item>}
+        <Grid.Column mobile={10} computer={14}>
+          {props.path !== "/" && <Menu.Item as={Link} to="/" >
+            <Image src={Logo} style={{ maxWidth: "5rem" }} />
+          </Menu.Item>}
 
-              </Grid.Column>
-              <Grid.Column mobile={6} computer={2} verticalAlign='middle'>
-                <Menu.Item as={Link} to="/blog">
-                  <Icon name="quote right" inverted></Icon>Blog
-         </Menu.Item>
-              </Grid.Column>
-            </Grid>
-          </Menu>
-        )
-      }}
-    />
+        </Grid.Column>
+        <Grid.Column mobile={6} computer={2} verticalAlign='middle'>
+          <Menu.Item as={Link} to="/blog">
+            <Icon name="quote right" inverted></Icon>Blog
+          </Menu.Item>
+        </Grid.Column>
+      </Grid>
+    </Menu>
   )
 }
 
