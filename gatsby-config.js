a446f2f6847b133634477a7629e786c68ae3ce83
EvaSpessotto <eva@spessotto.com>
2019-03-11 09:55:06 +0100

    Add CSP plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,21 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-react-helmet`,
     {
+      resolve: `gatsby-plugin-csp`,
+      options: {
+        disableOnDev: true,
+        mergeScriptHashes: true, // you can disable scripts sha256 hashes
+        mergeStyleHashes: true, // you can disable styles sha256 hashes
+        mergeDefaultDirectives: true,
+        directives: {
+          "script-src": "'self' www.google-analytics.com",
+          "style-src": "'self' 'unsafe-inline'",
+          "img-src": "'self' data: www.google-analytics.com"
+          // you can add your directives or override defaults
+        }
+      }
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: 'pages',
