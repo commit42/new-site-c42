8520179fd267eb87a3870c6487fdc94e864ddc56
EvaSpessotto <eva@spessotto.com>
2019-03-26 17:03:30 +0100

    Responsive homepage

--- src/components/homepage/HeaderIndex.js
+++ src/components/homepage/HeaderIndex.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import './headerIndex.scss';
 import { Image, Container, Grid, Header } from 'semantic-ui-react'
 
 const HeaderIndex = () => {
   return (
     <Grid>
-      <Grid.Row>
-        <Grid.Column width={8} >
+      <Grid.Row columns={2}>
+        <Grid.Column only='large screen computer'>
           <Image fluid style={{ maxHeight: '100vh' }} src="https://via.placeholder.com/800x1000" />
         </Grid.Column>
-        <Grid.Column width={8} as={Container} text style={{ marginTop: '70vh' }}>
+        <Grid.Column as={Container} text verticalAlign='middle'>
           <Header as="h1">Studio d'innovation web</Header>
           <p>Mollit nostrud tempor eiusmod duis in. Magna aliquip et reprehenderit sunt eu ad. Do cillum non sunt eu mollit anim magna pariatur amet velit.</p>
         </Grid.Column>
--- src/components/homepage/Services.js
+++ src/components/homepage/Services.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './services.scss';
 import { Grid, Header } from 'semantic-ui-react'
 
 const Services = () => {
@@ -8,41 +9,45 @@ const Services = () => {
         <Header as="h2">Découvrez tous nos services</Header>
       </Grid.Row>
       <Grid.Row style={{ marginTop: '8rem' }}>
-        <Grid.Column width={4}>
+        <Grid.Column mobile={14} tablet={14} computer={4}>
           <Grid textAlign='left'>
             <Grid.Row>
-              <Grid.Column width={1}>
+              <Grid.Column mobile={2} tablet={1} computer={2}>
                 <Header as="h3">
                   <i className="far fa-mobile"></i>
                 </Header>
               </Grid.Column>
-              <Grid.Column width={15}>
+              <Grid.Column mobile={14} tablet={15} computer={14}>
                 <Header as="h3">Web App PWA</Header>
                 <p>Ex ad sunt magna proident ullamco do velit. Laboris do nisi eu esse enim ullamco. Velit nisi nostrud sunt laborum tempor mollit adipisicing anim ut aliquip mollit. Nulla pariatur ex qui consequat ipsum exercitation culpa incididunt.</p>
               </Grid.Column>
             </Grid.Row>
           </Grid>
         </Grid.Column>
-        <Grid.Column width={4}>
+        <Grid.Column mobile={14} tablet={14} computer={4}>
           <Grid textAlign='left'>
             <Grid.Row>
-              <Grid.Column width={1}>
-                <Header as="h3"><i className="far fa-shopping-cart"></i></Header>
+              <Grid.Column mobile={2} tablet={1} computer={2}>
+                <Header as="h3">
+                  <i className="far fa-shopping-cart"></i>
+                </Header>
               </Grid.Column>
-              <Grid.Column width={15}>
+              <Grid.Column mobile={14} tablet={15} computer={14}>
                 <Header as="h3">E-commerce</Header>
                 <p>Ex ad sunt magna proident ullamco do velit. Laboris do nisi eu esse enim ullamco. Velit nisi nostrud sunt laborum tempor mollit adipisicing anim ut aliquip mollit. Nulla pariatur ex qui consequat ipsum exercitation culpa incididunt.</p>
               </Grid.Column>
             </Grid.Row>
           </Grid>
         </Grid.Column>
-        <Grid.Column width={4}>
+        <Grid.Column mobile={14} tablet={14} computer={4}>
           <Grid textAlign='left'>
             <Grid.Row>
-              <Grid.Column width={1}>
-                <Header as="h3"><i className="far fa-ruler-combined"></i></Header>
+              <Grid.Column mobile={2} tablet={1} computer={2}>
+                <Header as="h3">
+                  <i className="far fa-ruler-combined"></i>
+                </Header>
               </Grid.Column>
-              <Grid.Column width={15}>
+              <Grid.Column mobile={14} tablet={15} computer={14}>
                 <Header as="h3">Sites sur-mesure</Header>
                 <p>Ex ad sunt magna proident ullamco do velit. Laboris do nisi eu esse enim ullamco. Velit nisi nostrud sunt laborum tempor mollit adipisicing anim ut aliquip mollit. Nulla pariatur ex qui consequat ipsum exercitation culpa incididunt.</p>
               </Grid.Column>
