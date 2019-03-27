89f792efdae6095ea498d20eb393ec09920c0e55
EvaSpessotto <eva@spessotto.com>
2019-03-27 16:48:08 +0100

    add react-headroom for navbar

--- src/components/global/Nav.js
+++ src/components/global/Nav.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { Menu, Image, Grid, Container } from 'semantic-ui-react';
+import { Menu, Image, Container } from 'semantic-ui-react';
 import { Link } from 'gatsby'
+import Headroom from 'react-headroom';
 import Logo from '../../../static/assets/logo-noir-sansfond.png'
 
 
 const Nav = (props) => {
   return (
-    <Menu fixed='top' borderless inverted size="massive" style={{ backgroundColor: 'transparent', borderRadius: '0', margin: '0' }}>
+    <Headroom>
+    <Menu  borderless inverted size="massive" style={{ backgroundColor: 'white', borderRadius: '0', margin: '0' }}>
      <Container>
       <Menu.Item as={Link} to="/" >
             <Image src={Logo}/>
@@ -15,23 +17,8 @@ const Nav = (props) => {
         <h2 style={{ fontFamily: 'Poppins', color: 'black' }}>Blog</h2>
       </Menu.Item>
       </Container>
-
-      {/* <Grid container>
-        <Grid.Column mobile={10} computer={14}>
-          <Menu.Item as={Link} to="/" >
-            <Image src={Logo}/>
-          </Menu.Item>
-        </Grid.Column>
-        <Grid.Column mobile={6} computer={2} verticalAlign='middle'>
-          <Menu.Item as={Link} to="/blog">
-            <h2 style={{fontFamily:'Poppins', color:'black'}}>Blog</h2>
-          </Menu.Item>
-          <Menu.Item as={Link} to="/blog">
-            <h2 style={{fontFamily:'Poppins', color:'black'}}>Blog</h2>
-          </Menu.Item>
-        </Grid.Column>
-      </Grid> */}
     </Menu>
+    </Headroom>
   )
 }
 
