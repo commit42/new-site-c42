38dbd5e524f8a9d92d789e10f7821232198a3e2d
EvaSpessotto <eva@spessotto.com>
2019-04-03 17:14:29 +0200

    Update style testimonial cards

--- src/components/homepage/Testimonials.js
+++ src/components/homepage/Testimonials.js
@@ -1,31 +1,37 @@
 import React from "react"
-import { Grid, Icon, Header, Card } from "semantic-ui-react"
+import { Grid, Icon, Header, Card, Container } from "semantic-ui-react"
 import Fade from "react-reveal/Fade"
-
 
 const Testimonials = ({ data }) => {
   return (
-    <Grid
+    <Container
+      fluid
       style={{
         paddingTop: "5%",
         paddingBottom: "5%",
         backgroundColor: "#F9F9F9",
       }}
     >
-      <Grid.Row textAlign="center">
-        <Grid.Column>
-          <Fade bottom>
-            <Header as="h2">Ce que nos clients en pensent</Header>
-          </Fade>
-        </Grid.Column>
-      </Grid.Row>
-      <Grid.Row textAlign="center" style={{ marginTop: "5rem" }}>
-        <Grid.Column>
+      <Grid as={Container}>
+        <Grid.Row textAlign="center">
+          <Grid.Column>
+            <Fade bottom>
+              <Header as="h2">Ce que nos clients en pensent</Header>
+            </Fade>
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row
+          as={Container}
+          textAlign="center"
+          style={{ marginTop: "5rem" }}
+          centered
+        >
+          {/* <Grid.Column>
           <Card.Group centered>
             {data.testimonialsList.map((t, index) => {
               return (
                 <Fade  key={index}>
-                  <Card>
+                  <Card style={{margin:'0 10px 0 10px'}}>
                     <Card.Content>
                       <div>
                         {
@@ -40,7 +46,7 @@ const Testimonials = ({ data }) => {
                       >
                         {t.text}
                       </Card.Description>
-                      <Card.Header style={{ marginTop: "1rem" }}>
+                      <Card.Header style={{ marginTop: "1rem"}}>
                         {t.author}
                       </Card.Header>
                     </Card.Content>
@@ -49,12 +55,52 @@ const Testimonials = ({ data }) => {
               )
             })}
           </Card.Group>
-          <Header as="h4" style={{ marginTop: "3rem" }}>
-            <a href="https://www.google.com/search?q=commit42+avis&oq=commit42+avis&aqs=chrome..69i57.2474j0j7&sourceid=chrome&ie=UTF-8#lrd=0x12aebb873c60e57b:0x2ca3be697d36200,1,,,">Voir tous les avis <Icon name="long arrow alternate right" /></a>
-          </Header>
-        </Grid.Column>
-      </Grid.Row>
-    </Grid>
+        </Grid.Column> */}
+          {data.testimonialsList.map((t, index) => {
+            return (
+              <Grid.Column
+                style={{ marginBottom: "3rem" }}
+                key={index}
+                mobile={16}
+                tablet={8}
+                computer={5}
+              >
+                <Card fluid>
+                  <Card.Content>
+                    <div style={{display:'flex', justifyContent:'center'}}>
+                      {Array.apply(null, { length: t.rating }).map((e, i) => (
+                        <Icon name="star" color="yellow" key={i} />
+                      ))}
+                    </div>
+                    <Card.Description
+                      textAlign="left"
+                      style={{ marginTop: "2rem" }}
+                    >
+                      {t.text}
+                    </Card.Description>
+                  </Card.Content>
+
+                  <Card.Content extra >
+                    <Card.Header textAlign="center" >
+                      {t.author}
+                    </Card.Header>
+                  </Card.Content>
+                </Card>
+              </Grid.Column>
+            )
+          })}
+        </Grid.Row>
+        <Grid.Row>
+          <Grid.Column textAlign="center">
+            <Header as="h4" style={{ marginTop: "3rem" }}>
+              <a href="https://www.google.com/search?q=commit42+avis&oq=commit42+avis&aqs=chrome..69i57.2474j0j7&sourceid=chrome&ie=UTF-8#lrd=0x12aebb873c60e57b:0x2ca3be697d36200,1,,,">
+                Voir tous les avis <Icon name="long arrow alternate right" />
+              </a>
+            </Header>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </Container>
   )
 }
 
