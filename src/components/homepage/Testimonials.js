67b66ca3015ac3d0e240707379f9f3cb44633afb
EvaSpessotto <eva@spessotto.com>
2019-04-09 16:22:53 +0200

    Add breakpoints to slider

--- src/components/homepage/Testimonials.js
+++ src/components/homepage/Testimonials.js
@@ -27,6 +27,27 @@ const Testimonials = ({ data, isMobile }) => {
     spaceBetween: 80,
     autoplay: true,
     slidesPerView: "3",
+    breakpoints: {
+      // when window width is <= 320px
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 10,
+      },
+      // when window width is <= 480px
+      480: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      // when window width is <= 640px
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      1920: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+    },
   }
 
   return (
