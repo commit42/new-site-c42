61d979750eeeb2b2d8ec68ead1d9f74ce023675d
EvaSpessotto <eva@spessotto.com>
2019-04-09 11:39:42 +0200

    Fixbug theme's not working after build

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import "semantic-ui-css/semantic.min.css"
+// import "semantic-ui-css/semantic.min.css"
 import { graphql } from "gatsby"
 import SEO from "../components/SEO/SEO"
 import Layout from "../components/layout"
