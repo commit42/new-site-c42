609391a76164dec7c7f38babe48661fdf543cb19
EvaSpessotto <eva@spessotto.com>
2019-04-09 17:07:30 +0200

    Add better quality logo for navbar

--- src/components/global/Nav.js
+++ src/components/global/Nav.js
@@ -1,27 +1,31 @@
-import React from 'react';
-import { Menu, Image, Container, Header } from 'semantic-ui-react';
-import { Link } from 'gatsby'
-import Headroom from 'react-headroom';
-import Logo from '../../../static/assets/logo-noir-sansfond.png'
+import React from "react"
+import { Menu, Image, Container, Header } from "semantic-ui-react"
+import { Link } from "gatsby"
+import Headroom from "react-headroom"
+import Logo from "../../../static/assets/logo-c42.png"
 
-
-const Nav = (props) => {
+const Nav = props => {
   return (
     <Headroom>
-    <Menu  borderless inverted size="massive" style={{ backgroundColor: 'white', borderRadius: '0', margin: '0' }}>
-     <Container>
-      <Menu.Item as={Link} to="/" >
+      <Menu
+        borderless
+        inverted
+        size="massive"
+        style={{ backgroundColor: "white", borderRadius: "0", margin: "0" }}
+      >
+        <Container>
+          <Menu.Item as={Link} to="/" style={{ padding: "0" }}>
             <Image src={Logo} alt="Logo commit42" />
           </Menu.Item>
-      <Menu.Item as={Link} to="/blog" position="right">
-        <Header as="h3" style={{ fontFamily: 'Poppins', color: 'black' }}>Blog</Header>
-      </Menu.Item>
-      </Container>
-    </Menu>
+          <Menu.Item as={Link} to="/blog" position="right">
+            <Header as="h3" style={{ fontFamily: "Poppins", color: "black" }}>
+              Blog
+            </Header>
+          </Menu.Item>
+        </Container>
+      </Menu>
     </Headroom>
   )
 }
 
-
-
-export default Nav;
+export default Nav
