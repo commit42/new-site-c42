78c447b7039a1c56ee779ce046da65d87879be15
EvaSpessotto <eva@spessotto>
2019-04-10 14:25:46 +0200

    Fix: blogCard zIndex under navbar

--- src/components/global/Nav.js
+++ src/components/global/Nav.js
@@ -1,30 +1,34 @@
 import React from "react"
 import { Menu, Image, Container, Header } from "semantic-ui-react"
-import { Link } from "gatsby"
 import Headroom from "react-headroom"
+import { Link } from "gatsby"
 import Logo from "../../../static/assets/logo-c42.png"
 
 const Nav = props => {
   return (
-    <Headroom>
-      <Menu
-        borderless
-        inverted
-        size="massive"
-        style={{ backgroundColor: "white", borderRadius: "0", margin: "0" }}
-      >
-        <Container>
-          <Menu.Item as={Link} to="/" style={{ padding: "0" }}>
-            <Image src={Logo} alt="Logo commit42" />
-          </Menu.Item>
-          <Menu.Item as={Link} to="/blog" position="right">
-            <Header as="h3" style={{ fontFamily: "Poppins", color: "black" }}>
-              Blog
-            </Header>
-          </Menu.Item>
-        </Container>
-      </Menu>
-    </Headroom>
+    <Menu
+      fixed="top"
+      borderless
+      inverted
+      size="massive"
+      style={{
+        backgroundColor: "white",
+        borderRadius: "0",
+        margin: "0",
+        zIndex: "1",
+      }}
+    >
+      <Container>
+        <Menu.Item as={Link} to="/" style={{ padding: "0" }}>
+          <Image src={Logo} alt="Logo commit42" />
+        </Menu.Item>
+        <Menu.Item as={Link} to="/blog" position="right">
+          <Header as="h3" style={{ fontFamily: "Poppins", color: "black" }}>
+            Blog
+          </Header>
+        </Menu.Item>
+      </Container>
+    </Menu>
   )
 }
 
