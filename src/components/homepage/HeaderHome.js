f60a857c2a7d5c57ee469fd6c1fcca060a17a75e
EvaSpessotto <eva@spessotto>
2019-04-23 10:28:49 +0200

    Style: fixbug display img header homepage

--- src/components/homepage/HeaderHome.js
+++ src/components/homepage/HeaderHome.js
@@ -8,14 +8,21 @@ const HeaderHome = ({ data }) => {
   return (
     <Grid>
       <Grid.Row columns={2}>
-        <Grid.Column only="large screen computer">
+        <Grid.Column mobile={16} tablet={16} computer={8}>
           <Image
             style={{ maxHeight: "100vh" }}
             fluid={data.image.childImageSharp.fluid}
             alt="Motifs commit42"
           />
         </Grid.Column>
-        <Grid.Column as={Container} text verticalAlign="middle">
+        <Grid.Column
+          as={Container}
+          text
+          verticalAlign="middle"
+          mobile={16}
+          tablet={16}
+          computer={8}
+        >
           <div className="header-text-container">
             <Header as="h1">{data.heading}</Header>
             {formatText(data.description)}
