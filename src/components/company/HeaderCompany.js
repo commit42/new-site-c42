7f87ccc038108fdbc49449220612cff07a919393
EvaSpessotto <eva@spessotto>
2019-04-25 11:45:55 +0200

    Fixbug: img optimisation for header img

--- src/components/company/HeaderCompany.js
+++ src/components/company/HeaderCompany.js
@@ -1,11 +1,15 @@
 import React from "react"
 import "./CompanyPage.scss"
-import { Container, Image } from "semantic-ui-react"
+import Image from "gatsby-image"
+import { Container } from "semantic-ui-react"
 import img from "../../../static/assets/huehuehue.jpg"
 const HeaderCompany = ({ data }) => {
   return (
     <Container fluid className="header-comapny-img">
-      <Image src={img} fluid style={{ maxHeight: "500px" }} />
+      <Image
+        fluid={data.childImageSharp.fluid}
+        style={{ maxHeight: "500px" }}
+      />
     </Container>
   )
 }
