110e58a2675a4372d3f1398f98f94e2f11d8a0f6
EvaSpessotto <eva@spessotto>
2019-04-30 11:49:37 +0200

    Style: update carousel + style

--- src/components/company/OfficeCarousel.js
+++ src/components/company/OfficeCarousel.js
@@ -7,50 +7,6 @@ import { Container, Grid, Header, Button, Icon } from "semantic-ui-react"
 import Carousel from "nuka-carousel"
 
 const OfficeCarousel = ({ data }) => {
-  // const params = {
-  //   modules: [Pagination, Navigation],
-  //   spaceBetween: 30,
-  //   slidesPerView: 1,
-  //   rebuildOnUpdate: true,
-  //   pagination: {
-  //     el: ".swiper-pagination",
-  //     type: "bullets",
-  //     clickable: true,
-  //   },
-  //   breakpoints: {
-  //     320: {
-  //       slidesPerView: 1,
-  //       spaceBetween: 10,
-  //     },
-  //     480: {
-  //       slidesPerView: 1,
-  //       spaceBetween: 20,
-  //     },
-  //     640: {
-  //       slidesPerView: 1,
-  //       spaceBetween: 20,
-  //     },
-  //     1920: {
-  //       slidesPerView: 1,
-  //       spaceBetween: 20,
-  //     },
-  //   },
-  //   navigation: {
-  //     nextEl: ".swiper-button-next",
-  //     prevEl: ".swiper-button-prev",
-  //   },
-  //   renderPrevButton: () => (
-  //     <Button className="swiper-button-prev">
-  //       <Icon name="arrow left" />
-  //     </Button>
-  //   ),
-  //   renderNextButton: () => (
-  //     <Button className="swiper-button-next">
-  //       <Icon name="arrow right" />
-  //     </Button>
-  //   ),
-  // }
-
   return (
     <Container className="primary office-carousel" fluid>
       <Grid as={Container}>
@@ -63,25 +19,10 @@ const OfficeCarousel = ({ data }) => {
           </Grid.Column>
         </Grid.Row>
         <Grid.Row style={{ minHeight: "700px" }}>
-          {/* <Swiper {...params}>
-            {data.pictures &&
-              data.pictures.map((picture, index) => {
-                return (
-                  <Grid.Column
-                    style={{ marginBottom: "3rem" }}
-                    mobile={16}
-                    tablet={16}
-                    computer={5}
-                    textAlign="center"
-                    key={index}
-                  >
-                    <Image fluid={picture.picture.childImageSharp.fluid} />
-                  </Grid.Column>
-                )
-              })}
-          </Swiper> */}
-
           <Carousel
+            enableKeyboardControls={true}
+            dragging={true}
+            swiping={true}
             renderCenterLeftControls={({ previousSlide }) => (
               <Button className="swiper-button-prev" onClick={previousSlide}>
                 <Icon name="arrow left" />
