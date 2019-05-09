596057fd5dcd2a24860edbf532e5f32d341b39a4
EvaSpessotto <eva@spessotto>
2019-05-09 15:35:16 +0200

    Feat: add searchbar but only works on page 1 of pagiantion

--- src/templates/PostsListTemplate.js
+++ src/templates/PostsListTemplate.js
@@ -2,10 +2,21 @@ import React from "react"
 import "./PostsListTemplate.scss"
 import { graphql, Link } from "gatsby"
 import Image from "gatsby-image"
-import { Container, Grid, Icon, Card, Label, Header } from "semantic-ui-react"
+import {
+  Container,
+  Grid,
+  Icon,
+  Card,
+  Label,
+  Header,
+  Input,
+} from "semantic-ui-react"
 import Fade from "react-reveal/Fade"
 import moment from "moment"
+import _ from "lodash"
+
 import Layout from "../components/Layout"
+import SEO from "../components/seo/SEO"
 import HeroBlog from "../components/blog/HeroBlog"
 
 const NavLink = props => {
@@ -24,94 +35,122 @@ const NavLink = props => {
   }
 }
 
-const IndexPage = ({ pageContext, data }) => {
-  const { group, index, first, last, pageCount } = pageContext
-  const previousUrl = index - 1 === 1 ? "" : (index - 1).toString()
-  const nextUrl = (index + 1).toString()
+class PostsListTemplate extends React.Component {
+  state = { value: "" }
+  onchangeSearch(e) {
+    this.setState({ value: e.target.value })
+  }
+  render() {
+    const { group, index, first, last, pageCount } = this.props.pageContext
+    const previousUrl = index - 1 === 1 ? "" : (index - 1).toString()
+    const nextUrl = (index + 1).toString()
 
-  const blogData = data.markdownRemark.frontmatter
+    const blogData = this.props.data.markdownRemark.frontmatter
 
-  return (
-    <Layout>
-      <HeroBlog data={blogData} />
-      <Container fluid className="primary" id="posts-list-container">
-        <Grid as={Container}>
-          <Grid.Row>
-            {group.map(({ node: post }, index) => {
-              const altText =
-                post.frontmatter.thumbnail &&
-                post.frontmatter.thumbnail.childImageSharp.fluid.originalName.slice(
-                  0,
-                  -4
-                )
-              return (
-                <Grid.Column
-                  mobile={16}
-                  tablet={8}
-                  computer={5}
-                  className="blogcard"
-                  key={index}
-                >
-                  <Fade cascade>
-                    <Card fluid as={Link} to={post.fields.slug}>
-                      {post.frontmatter.thumbnail != null && (
-                        <Image
-                          fluid={
-                            post.frontmatter.thumbnail.childImageSharp.fluid
-                          }
-                          alt={altText}
-                        />
-                      )}
+    return (
+      <Layout>
+        <SEO
+          title="Blog | commit42"
+          description="Découvrez tous les articles de commit42 !"
+          pathname="/blog"
+        />
+        <HeroBlog data={blogData} />
+        <Container fluid className="primary" id="posts-list-container">
+          <Grid as={Container}>
+            <Grid.Row>
+              <Input
+                type="search"
+                icon="search"
+                onChange={e => this.onchangeSearch(e)}
+              />
+            </Grid.Row>
+            <Grid.Row>
+              {group
+                .filter(({ node: item }) => {
+                  console.log(item.frontmatter.title.match(this.state.value))
+                  return (
+                    item.frontmatter.title
+                      .toLowerCase()
+                      .match(this.state.value) ||
+                    item.excerpt.toLowerCase().match(this.state.value)
+                  )
+                })
+                .map(({ node: post }, index) => {
+                  const altText =
+                    post.frontmatter.thumbnail &&
+                    post.frontmatter.thumbnail.childImageSharp.fluid.originalName.slice(
+                      0,
+                      -4
+                    )
+                  return (
+                    <Grid.Column
+                      mobile={16}
+                      tablet={8}
+                      computer={5}
+                      className="blogcard"
+                      key={index}
+                    >
+                      <Fade cascade>
+                        <Card fluid as={Link} to={post.fields.slug}>
+                          {post.frontmatter.thumbnail != null && (
+                            <Image
+                              fluid={
+                                post.frontmatter.thumbnail.childImageSharp.fluid
+                              }
+                              alt={altText}
+                            />
+                          )}
 
-                      <Card.Content>
-                        <Card.Header>{post.frontmatter.title}</Card.Header>
-                        <Card.Meta className="mt-1">
-                          <span>
-                            {moment(post.frontmatter.date).format(
-                              "Do MMM YYYY"
-                            )}
-                          </span>
-                          <span> • {post.timeToRead}min à perdre</span>
-                        </Card.Meta>
-                        <Card.Description>{post.excerpt}</Card.Description>
-                      </Card.Content>
+                          <Card.Content>
+                            <Card.Header>{post.frontmatter.title}</Card.Header>
+                            <Card.Meta className="mt-1">
+                              <span>
+                                {moment(post.frontmatter.date).format(
+                                  "Do MMM YYYY"
+                                )}
+                              </span>
+                              <span> • {post.timeToRead}min à perdre</span>
+                            </Card.Meta>
+                            <Card.Description>{post.excerpt}</Card.Description>
+                          </Card.Content>
 
-                      {post.frontmatter.tags && (
-                        <Card.Content extra>
-                          {post.frontmatter.tags.map((tag, index) => (
-                            <Label key={index}>{tag}</Label>
-                          ))}
-                        </Card.Content>
-                      )}
-                    </Card>
-                  </Fade>
-                </Grid.Column>
-              )
-            })}
-          </Grid.Row>
-          <Grid.Row textAlign="center" className="posts-list-pagination">
-            <Grid.Column>
-              <span className="previousLink">
-                <NavLink
-                  test={first}
-                  url={`/blog/${previousUrl}`}
-                  text="prev"
-                />
-              </span>
-              <Header as="h3" className="pagecount">
-                {index} / {pageCount}
-              </Header>
-              <span className="nextLink">
-                <NavLink test={last} url={`/blog/${nextUrl}`} text="next" />
-              </span>
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </Container>
-    </Layout>
-  )
+                          {post.frontmatter.tags && (
+                            <Card.Content extra>
+                              {post.frontmatter.tags.map((tag, index) => (
+                                <Label key={index}>{tag}</Label>
+                              ))}
+                            </Card.Content>
+                          )}
+                        </Card>
+                      </Fade>
+                    </Grid.Column>
+                  )
+                })}
+            </Grid.Row>
+            <Grid.Row textAlign="center" className="posts-list-pagination">
+              <Grid.Column>
+                <span className="previousLink">
+                  <NavLink
+                    test={first}
+                    url={`/blog/${previousUrl}`}
+                    text="prev"
+                  />
+                </span>
+                <Header as="h3" className="pagecount">
+                  {index} / {pageCount}
+                </Header>
+                <span className="nextLink">
+                  <NavLink test={last} url={`/blog/${nextUrl}`} text="next" />
+                </span>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </Container>
+      </Layout>
+    )
+  }
 }
-export default IndexPage
+export default PostsListTemplate
 
 export const BlogPageQuery = graphql`
   query BlogPageQuery {
