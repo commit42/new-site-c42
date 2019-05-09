66e8e388c79c8550ab696da1261b65da21e4072c
EvaSpessotto <eva@spessotto>
2019-05-09 10:30:23 +0200

    Style: responsive + style page ecommerce

--- src/components/ecommerce/HeroEcommerce.js
+++ src/components/ecommerce/HeroEcommerce.js
@@ -1,22 +1,36 @@
 import React from "react"
 import "./Ecommerce.scss"
 import { Container, Grid, Header } from "semantic-ui-react"
+import Fade from "react-reveal/Fade"
 
 const HeroEcommerce = () => {
   return (
     <Grid as={Container} verticalAlign="middle" id="hero-ecommerce-container">
       <Grid.Row>
         <Grid.Column computer={8} tablet={8} mobile={16}>
-          <Header as="h1">E-commerce</Header>
-          <p>
-            Nulla enim officia excepteur consequat sit. Sit excepteur veniam
-            aliqua consequat dolore tempor. Consequat exercitation excepteur
-            proident dolore aliquip. Ea do ipsum mollit magna occaecat dolor qui
-            magna. Occaecat nulla non Lorem duis elit dolor ea Lorem.
-          </p>
+          <Fade top cascade>
+            <Header as="h1" className="hero-ecommerce-heading">
+              E-commerce
+            </Header>
+          </Fade>
+          <Fade left>
+            <p>
+              Nulla enim officia excepteur consequat sit. Sit excepteur veniam
+              aliqua consequat dolore tempor. Consequat exercitation excepteur
+              proident dolore aliquip. Ea do ipsum mollit magna occaecat dolor
+              qui magna. Occaecat nulla non Lorem duis elit dolor ea Lorem.
+            </p>
+          </Fade>
         </Grid.Column>
-        <Grid.Column computer={8} tablet={8} mobile={16}>
-          <img src="http://via.placeholder.com/640x360" />
+        <Grid.Column
+          computer={8}
+          tablet={8}
+          mobile={16}
+          className="hero-ecommerce-img"
+        >
+          <Fade top>
+            <img src="http://via.placeholder.com/640x360" />
+          </Fade>
         </Grid.Column>
       </Grid.Row>
     </Grid>
--- src/components/ecommerce/ReferencesEcommerce.js
+++ src/components/ecommerce/ReferencesEcommerce.js
@@ -1,19 +1,24 @@
 import React from "react"
 import "./Ecommerce.scss"
 import { Container, Grid, Header } from "semantic-ui-react"
+import Fade from "react-reveal/Fade"
 
 const HeroEcommerce = () => {
   return (
     <Container fluid className="primary" id="references-ecommerce-container">
       <Grid as={Container}>
         <Grid.Row>
-          <Grid.Column>
-            <Header as="h2">Nos références E-commerce</Header>
+          <Grid.Column className="references-ecommerce-heading">
+            <Fade>
+              <Header as="h2">Nos références E-commerce</Header>
+            </Fade>
           </Grid.Column>
         </Grid.Row>
         <Grid.Row>
           <Grid.Column computer={8} tablet={8} mobile={16}>
-            <img src="http://via.placeholder.com/640x360" />
+            <Fade>
+              <img src="http://via.placeholder.com/640x360" />
+            </Fade>
           </Grid.Column>
           <Grid.Column
             computer={8}
@@ -21,13 +26,19 @@ const HeroEcommerce = () => {
             mobile={16}
             verticalAlign="bottom"
           >
-            <Header as="h3">Jumbobag</Header>
-            <p>
-              Consequat aliquip deserunt ut velit sint adipisicing eiusmod
-              fugiat enim non. Aute cupidatat cupidatat consectetur ad
-              consectetur cupidatat amet voluptate do est commodo. Aliqua minim
-              in qui cupidatat ut exercitation deserunt cillum Lorem.
-            </p>
+            <Fade>
+              <Header as="h3" className="reference-item-heading">
+                Jumbobag
+              </Header>
+            </Fade>
+            <Fade>
+              <p>
+                Consequat aliquip deserunt ut velit sint adipisicing eiusmod
+                fugiat enim non. Aute cupidatat cupidatat consectetur ad
+                consectetur cupidatat amet voluptate do est commodo. Aliqua
+                minim in qui cupidatat ut exercitation deserunt cillum Lorem.
+              </p>
+            </Fade>
           </Grid.Column>
         </Grid.Row>
       </Grid>
