1443a298f6d7e21f33c70f1ca70462ba778d901b
EvaSpessotto <eva@spessotto>
2019-05-10 17:03:05 +0200

    Add dynamic datas for navbar and sidebar

--- src/components/global/Sidebar.js
+++ src/components/global/Sidebar.js
@@ -1,60 +1,68 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql, StaticQuery } from "gatsby"
 import { Header, Menu, Icon } from "semantic-ui-react"
+import kebabCase from "lodash/kebabCase"
 import Drawer from "react-motion-drawer"
 import Bounce from "react-reveal/Bounce"
 import Logo from "../../../static/assets/logo-c42.png"
 
 const Sidebar = ({ isOpen, setIsOpen }) => {
   return (
-    <div id="sidebar-container">
-      <Drawer
-        width={250}
-        fadeOut
-        open={isOpen}
-        onChange={() => setIsOpen(!isOpen)}
-        className="sidebar"
-      >
-        <div className="sidebar--container">
-          <Bounce left>
-            <Link to="/pwa">
-              <Header as="h3" className="menu-item ">
-                <Icon name="mobile alternate" color="grey" size="big" />
-                PWA
-              </Header>
-            </Link>
-            <Link to="/e-commerce">
-              <Header as="h3" className="menu-item">
-                <Icon name="cart" color="grey" size="big" />
-                E-commerce
-              </Header>
-            </Link>
-            <Link to="/blog">
-              <Header as="h3" className="menu-item ">
-                <Icon name="quote right" color="grey" size="big" />
-                Blog
-              </Header>
-            </Link>
-            <Link to="/la-societe">
-              <Header as="h3" className="menu-item">
-                <Icon name="users" color="grey" size="big" />
-                La Societe
-              </Header>
-            </Link>
-          </Bounce>
-        </div>
-      </Drawer>
-      <Menu fixed="top" borderless>
-        <Menu.Item onClick={() => setIsOpen(!isOpen)}>
-          <Icon name="bars" color="grey" size="big" />
-        </Menu.Item>
-        <Menu.Item position="right">
-          <Link to="/">
-            <img src={Logo} alt="Logo commit42" />
-          </Link>
-        </Menu.Item>
-      </Menu>
-    </div>
+    <StaticQuery
+      query={graphql`
+        query sidebarQuery {
+          markdownRemark(frontmatter: { pageName: { eq: "navbar" } }) {
+            frontmatter {
+              pageName
+              logo
+              pageList {
+                name
+                icon
+              }
+            }
+          }
+        }
+      `}
+      render={data => {
+        const sidebarDatas = data.markdownRemark.frontmatter
+        return (
+          <div id="sidebar-container">
+            <Drawer
+              width={250}
+              fadeOut
+              open={isOpen}
+              onChange={() => setIsOpen(!isOpen)}
+              className="sidebar"
+            >
+              <div className="sidebar--container">
+                <Bounce left>
+                  {sidebarDatas.pageList.map(item => {
+                    return (
+                      <Link to={`/${kebabCase(item.name)}`}>
+                        <Header as="h3" className="menu-item ">
+                          <Icon name={item.icon} color="grey" size="big" />
+                          {item.name}
+                        </Header>
+                      </Link>
+                    )
+                  })}
+                </Bounce>
+              </div>
+            </Drawer>
+            <Menu fixed="top" borderless>
+              <Menu.Item onClick={() => setIsOpen(!isOpen)}>
+                <Icon name="bars" color="grey" size="big" />
+              </Menu.Item>
+              <Menu.Item position="right">
+                <Link to="/">
+                  <img src={sidebarDatas.logo} alt="Logo commit42" />
+                </Link>
+              </Menu.Item>
+            </Menu>
+          </div>
+        )
+      }}
+    />
   )
 }
 
